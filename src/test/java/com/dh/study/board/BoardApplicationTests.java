4ec3479d7fa7653a7e9ac41e3dd49d6d3c181abb
donghwi <cksael@gmail.com>
2025-06-06 23:57:27 +0900

    기본 java 클래스 커밋

---
+++ src/test/java/com/dh/study/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.study.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
