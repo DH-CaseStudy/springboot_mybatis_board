3641ade78614d3cdb65374862302a3a6de69884e
donghwi <cksael@gmail.com>
2025-06-08 14:21:44 +0900

    게시글 리스트 출력

--- src/main/java/com/dh/study/board/controller/BoardController.java
+++ src/main/java/com/dh/study/board/controller/BoardController.java
@@ -4,8 +4,11 @@ import com.dh.study.board.dto.BoardDTO;
 import com.dh.study.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,6 +23,15 @@ public class BoardController {
     @PostMapping("save")
     public String save(BoardDTO boardDTO) {
         System.out.println("board DTO = " + boardDTO.toString());
+        boardService.save(boardDTO);
         return "index";
+    }
+
+    @GetMapping("/list")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        System.out.println("boardDTOList = " + boardDTOList);
+        return "list";
     }
 }
---
+++ src/main/java/com/dh/study/board/dto/BoardFileDTO.java
@@ -0,0 +1,4 @@
+package com.dh.study.board.dto;
+
+public class BoardFileDTO {
+}
